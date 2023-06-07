9f091547a89beb9dbfc98ce8911f384cd84a7e3a
thomasrouiller <thomas.rouiller@studentfr.ch>
2023-06-07 11:07:00 +0200

    navigation done html css js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
 import gsap from "gsap";
+
+var button = document.querySelector(".navigation-close");
+var menu = document.querySelector(".navigation-open");
+var navigation = document.querySelector(".navigation-credits-informations");
+
+var openMenu = function () {
+  menu.classList.toggle("is-open");
+  button.classList.toggle("is-active");
+  navigation.classList.toggle("is-open");
+};
+
+button.addEventListener("click", openMenu);
+
+links.forEach((link) => {
+  link.addEventListener("click", openMenu);
+});
